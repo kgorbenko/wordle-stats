50b22690e9918e9945871c169592731f268001f0
Kirill Gorbenko <kgorbenkodev@proton.me>
2024-01-12 06:26:42 +0300

    Add status endpoint

--- WordleStats/Program.fs
+++ WordleStats/Program.fs
@@ -1,13 +1,34 @@
 open System
+open System.Diagnostics
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Hosting
+
+type ApplicationStatus = {
+    Name: string
+    Version: string
+    Environment: string
+}
+
+let getVersion () =
+    FileVersionInfo
+        .GetVersionInfo(Environment.GetCommandLineArgs()[0])
+        .FileVersion
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/status", Func<IWebHostEnvironment, ApplicationStatus>(
+        fun (webHostEnvironment: IWebHostEnvironment) ->
+            {
+                Name = webHostEnvironment.ApplicationName
+                Version = getVersion ()
+                Environment = webHostEnvironment.EnvironmentName
+            }
+        )
+    ) |> ignore
 
     app.Run()
 
